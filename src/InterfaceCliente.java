b272e5edf145f666261292b1723a2749e8c308dd
ruialmeida96 <34514089+ruialmeida96@users.noreply.github.com>
2018-05-23 12:18:28 +0100

    teste rui

--- src/InterfaceCliente.java
+++ src/InterfaceCliente.java
@@ -116,7 +116,7 @@ public class InterfaceCliente extends javax.swing.JFrame {
             }
         });
 
-        jTextField3.setText("jTextField3");
+        jTextField3.setText("teste");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
